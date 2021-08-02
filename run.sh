88301e7e067f98a9ab7a9354a58e7fa711d9a34d
XiangPan <xiangpan@nyu.edu>
2021-08-02 16:39:34 -0400

    add script

---
+++ run.sh
@@ -0,0 +1,13 @@
+###
+ # @Author: Xiang Pan
+ # @Date: 2021-08-02 16:38:49
+ # @LastEditTime: 2021-08-02 16:39:27
+ # @LastEditors: Xiang Pan
+ # @Description: 
+ # @FilePath: /HOBO/run.sh
+ # xiangpan@nyu.edu
+### 
+python grid_search_HNSW
+python grid_search_IVF_FLAT
+python grid_search_IVF_PQ
+python grid_search_IVF_SQ8
